e37cd0f536babda84c5c66525afe12d46592a4ee
Vishwajit Mahesh Bavadhankar <vishub2004@gmail.com>
2026-07-28 15:57:46 +0530

    Add files via upload

---
+++ Inc/gpio.h
@@ -0,0 +1,9 @@
+#ifndef GPIO_H
+#define GPIO_H
+
+void GPIO_Init(void);
+void LED_ON(void);
+void LED_OFF(void);
+void LED_Toggle(void);
+
+#endif
---
+++ Inc/tasks.h
@@ -0,0 +1,7 @@
+#ifndef TASKS_H
+#define TASKS_H
+
+void Task1(void *pvParameters);
+void Task2(void *pvParameters);
+
+#endif
